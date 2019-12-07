49e08fbc4b12ec7a9e8edb46ebbe17a3da941f11
thiagohubes <thiago.hubes@gmail.com>
2019-12-07 17:36:52 -0300

    Adicionado arquivo Variables and Datatypes

---
+++ A100_Variables_and_Datatypes.java
@@ -0,0 +1,11 @@
+class Test {
+    int x = 50;             //instance variable
+
+    static int y = 100;     //class variable
+
+    void my_Method() {
+
+        int z = 150;        //local variable
+
+    }
+}
